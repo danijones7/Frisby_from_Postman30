2a234e472a5726ac7bb0e421f8b915120477e814
Jessie <jessica.v.rid@gmail.com>
2023-06-26 19:36:52 +0300

    init commit

---
+++ d00.js
@@ -0,0 +1,13 @@
+const frisby = require("frisby");
+
+describe("d00", () => {
+    it("Check status, args", function () {
+        return frisby
+            .post("https://postman-echo.com/post?set_start_day=June 25, 2023")
+            .expect("status", 200)
+            .expect("json", "args", { set_start_day: "June 25, 2023" },)
+        // .then((result) => {
+        //     console.log(result.json);
+        // });
+    });
+});
---
+++ d01.js
@@ -0,0 +1,14 @@
+const frisby = require("frisby");
+// const Joi = frisby.Joi;
+describe("d00", () => {
+    it("Check status, data, args", function () {
+        return frisby
+            .post("https://postman-echo.com/post", { "payload": "hello world" })
+            .expect("status", 200)
+            .expect("json", "data", { payload: "hello world" })
+
+            .then((result) => {
+                console.log(result.json);
+            });
+    });
+});
