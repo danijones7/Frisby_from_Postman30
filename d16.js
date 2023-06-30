40afcb7d28c7b4272cebfe70f993d98057a34f3c
Jessie <jessica.v.rid@gmail.com>
2023-07-01 00:00:12 +0300

    day 16 fixed

--- d16.js
+++ d16.js
@@ -1,36 +1,23 @@
-// const frisby = require('frisby')
+const frisby = require('frisby')
+jest.setTimeout(10000000);
 
-// describe('d16', () => {
-//     it('Pagination', async function () {
-//         let page = 1;
-//         let status = 200;
-//         while (status == 200) {
-//             const response = await frisby
-//                 .get(`http://xkcd.com/${page}/info.0.json`)
-//                 .expect('status', 200)
-//             status = response.status
-//             console.log(`Current page is: ${page}`);
-//             page++;
-//             // console.log(`Next page is: ${page}`)
-//         }
-//     });
-// });
+describe('d16', () => {
+    it('Pagination', async function () {
+        let page = 2790;
+        let status = 200;
 
+        while (true) {
+            const response = await frisby
+                .get(`http://xkcd.com/${page}/info.0.json`)
+            status = response.status
+            if (status == 404) {
+                break;
+            }
+            console.log(`Current page is: ${page}`);
+            page++;
+            console.log(`Next page is: ${page}`)
+        }
 
-// const frisby = require("frisby");
-// jest.setTimeout(10000000);
-
-// describe("Day 16", () => {
-//     it("Listing web pages", async () => {
-//         let page = 0;
-//         let status = 200;
-
-//         while (status == 200) {
-//             page++;
-//             const response = await frisby.get(`http://xkcd.com/${page}/info.0.json`);
-//             status = response.status;
-//         }
-//         console.log(page);
-//         expect(page).toEqual(404);
-//     });
-// });
+        expect(page).toEqual(2797);
+    });
+});
